7d752e37cebf6226531e78e6018254e5b5b93cbb
Talles <tallesfelixg95@gmail.com>
2016-10-29 19:34:04 -0300

    teste8

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -11,7 +11,12 @@ server '45.55.172.126', roles: [:web, :app, :db], primary: true
 # This can be used to drop a more detailed server definition into the
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
+set :ssh_options, {
 
+  forward_agent: false,
+  #:verbose => :debug
+
+}
 
 
 # Custom SSH Options
