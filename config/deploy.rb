4644383431e71880fc710e8d1009fade61513765
Talles <tallesfelixg95@gmail.com>
2016-10-29 18:27:47 -0300

    teste3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 
 # Change these
 server '45.55.172.126', port: 22, roles: [:web, :app, :db], primary: true
-=======
-# Change these
-server '45.55.172.126', port: 8080, roles: [:web, :app, :db], primary: true
->>>>>>> f15414157c2e178e1ce9a52c2759283917a6ad8a
 
 set :repo_url,        'git@github.com:GTi-Jr/sistema-hotel-fluxo.git'
 set :application,     'sistema-hotel-fluxo'
@@ -16,23 +11,14 @@ set :puma_workers,    0
 # Don't change these unless you know what you're doing
 set :pty,             true
 set :use_sudo,        false
-<<<<<<< HEAD
 set :stage,           :production
-=======
-set :stages,          :production
->>>>>>> f15414157c2e178e1ce9a52c2759283917a6ad8a
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
 set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
-<<<<<<< HEAD
 set :puma_access_log, "#{release_path}/log/puma.access.log"
 set :puma_error_log,  "#{release_path}/log/puma.error.log"
-=======
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
->>>>>>> f15414157c2e178e1ce9a52c2759283917a6ad8a
 set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
@@ -40,11 +26,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 
 ## Defaults:
 # set :scm,           :git
-<<<<<<< HEAD
 # set :branch,        :master
-=======
- set :branch,        :deploy
->>>>>>> f15414157c2e178e1ce9a52c2759283917a6ad8a
 # set :format,        :pretty
 # set :log_level,     :debug
 # set :keep_releases, 5
