7cd4d73c511ca6f73f8f24054e3f1bd5a5607916
Talles <tallesfelixg95@gmail.com>
2016-10-29 21:12:30 -0300

    teste30

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,6 @@ set :puma_workers,    0
 
 # Don't change these unless you know what you're doing
 set :pty,             true
-set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
