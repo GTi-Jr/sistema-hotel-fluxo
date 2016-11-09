bfdfb11df2d9ce8a6e06da1c0fc3792934b0afbe
Caio Ergos <caioergos@gmail.com>
2016-11-08 22:39:34 -0300

    relação entre produto e transação

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,4 @@
 class Product < ActiveRecord::Base
   belongs_to :sector
   belongs_to :department
-
-
- 
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,29 +1,27 @@
 class Transaction < ActiveRecord::Base
+  belongs_to :product, foreign_key: 'product_code', primary_key: 'code'
+  belongs_to :employee
+
   def self.main_query(options = {})
 
-  	#VERIFICAR SE É HOSPEDAGEM
-  	product_hospedagem_code = Product.find_by(name: 'Hospedagem').code
-  	if product_hospedagem_code==options[:code].to_i
-  		#verifica sem o campo code_client e price não está vazio
-  		if options[:client_code].blank? || options[:value].blank?
+      #VERIFICAR SE É HOSPEDAGEM
+      product_hospedagem_code = Product.find_by(name: 'Hospedagem').code
+      if product_hospedagem_code==options[:code].to_i
+          #verifica sem o campo code_client e price não está vazio
+          if options[:client_code].blank? || options[:value].blank?
         return {status: false, message: "Parece que você não preencheu alguns campos obrigatórios."}
       end
     end
 
 
     #salvar dados
-    @transaction_new = Transaction.new(type_t: options[:type_t], 
-     quantity: options[:quantity], client_code:options[:client_code], 
+    @transaction_new = Transaction.new(type_t: options[:type_t],
+     quantity: options[:quantity], client_code:options[:client_code],
      price: options[:value], product_code: options[:code],employee_id: options[:employee], data_t: options[:data_trans])
     if @transaction_new.save!
       return {status: true, message: @transaction_new.id}
-    else 
+    else
       return {status: false, message: "Erro S."}
     end
-
-
-
   end
-
-
 end
