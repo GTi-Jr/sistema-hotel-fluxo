6d17e9e6528827b3af54e740832b4202e44de21c
Caio Ergos <caioergos@gmail.com>
2016-11-08 23:39:59 -0300

    melhoramento do teste

--- spec/models/history_query_spec.rb
+++ spec/models/history_query_spec.rb
@@ -17,8 +17,20 @@ RSpec.describe HistoryQuery, :type => :model do
     transactions << product_5 = Transaction.create!(product_code: 555, data_t: Time.zone.now.to_date)
 
     range_1 = "#{Time.zone.now.to_date.yesterday.strftime('%d/%m/%Y')} - #{Time.zone.now.to_date.strftime('%d/%m/%Y')}"
+    range_2 = "#{Time.zone.now.to_date.yesterday.yesterday.strftime('%d/%m/%Y')} - #{Time.zone.now.to_date.tomorrow.strftime('%d/%m/%Y')}"
+    range_3 = "#{Time.zone.now.to_date.strftime('%d/%m/%Y')} - #{Time.zone.now.to_date.strftime('%d/%m/%Y')}"
 
-    expect(Transaction.where(data_t: (Time.zone.now.to_date)..(Time.zone.now.to_date.tomorrow)).order(:product_code).to_a).to eq([product_3, product_4, product_5])
     expect(HistoryQuery.main_query(date_range: range_1).order(:product_code).to_a).to eq([product_1, product_4, product_5])
+    expect(HistoryQuery.main_query(date_range: range_2).order(:product_code).to_a).to eq(transactions)
+    expect(HistoryQuery.main_query(date_range: range_3).order(:product_code).to_a).to eq([product_4, product_5])
+  end
+
+  it 'should search by type' do
+    transactions = []
+    transactions << product_1 = Transaction.create!(product_code: 111, data_t: Time.zone.now.to_date.yesterday)
+    transactions << product_2 = Transaction.create!(product_code: 222, data_t: Time.zone.now.to_date.yesterday.yesterday)
+    transactions << product_3 = Transaction.create!(product_code: 333, data_t: Time.zone.now.to_date.tomorrow)
+    transactions << product_4 = Transaction.create!(product_code: 444, data_t: Time.zone.now.to_date)
+    transactions << product_5 = Transaction.create!(product_code: 555, data_t: Time.zone.now.to_date)
   end
 end
