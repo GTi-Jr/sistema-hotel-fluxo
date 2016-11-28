027d37c9be78d9c4382b20c344cc9ace94b0fd0f
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2016-11-27 22:33:55 -0300

    fiz production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ction_Mailer::Base.smtp_settings = {
+  Action_Mailer::Base.smtp_settings = {
     :address              => 'smtp.sendgrid.net',
     :port                 => 587,
     :domain               => 'rubysnack.com',
