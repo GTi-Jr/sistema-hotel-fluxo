4bf1c4475e58a18f167c866cb4c84b583d98457b
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2016-12-04 16:43:33 -0300

    deploy thin

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,20 +13,6 @@ set :deploy_to, '/home/rails/sistema_hotel'
 set :use_sudo, false
 set :stage, :production
 
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
-
-
-set :puma_bind,       "unix:///home/rails/sistema_hotel/shared/tmp/sockets/sistema_hotel-puma.sock"
-set :puma_state,      "unix:///home/rails/sistema_hotel/shared/tmp/pids/puma.state"
-set :puma_pid,        "unix:///home/rails/sistema_hotel/shared/tmp/pids/puma.pid"
-set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :puma_preload_app, true
-set :puma_worker_timeout, nil
-set :puma_init_active_record, true
-
-
 # Default value for :scm is :git
 # set :scm, :git
 
@@ -53,42 +39,6 @@ set :puma_init_active_record, true
 # set :keep_releases, 5
 
 set :linked_files, %w{config/database.yml config/application.yml}
-
-namespace :puma do
-  desc 'Create Directories for Puma Pids and Socket'
-  task :make_dirs do
-    on roles(:app) do
-      execute "mkdir #{shared_path}/tmp/sockets -p"
-      execute "mkdir #{shared_path}/tmp/pids -p"
-    end
-  end
-
-  before :start, :make_dirs
-end
-
-namespace :deploy do
-  desc "Make sure local git is in sync with remote."
-  task :check_revision do
-    on roles(:app) do
-      unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        puts "WARNING: HEAD is not the same as origin/master"
-        puts "Run `git push` to sync changes."
-        exit
-      end
-    end
-  end
-
-  desc 'Initial Deploy'
-  task :initial do
-    on roles(:app) do
-      before 'deploy:restart', 'puma:start'
-      invoke 'deploy'
-    end
-  end
-
-  before :starting,     :check_revision
-  after  :finishing,    :cleanup
-end
 
 desc 'Restart application'
 task :restart do
