1ce06bdc7550380bfa267cf1b5fd73c77223f02f
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2016-12-04 16:34:42 -0300

    deploy puma

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,9 +17,9 @@ set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
 
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/sistema_hotel-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_bind,       "unix:///home/rails/sistema_hotel/shared/tmp/sockets/sistema_hotel-puma.sock"
+set :puma_state,      "unix:///home/rails/sistema_hotel/shared/tmp/pids/puma.state"
+set :puma_pid,        "unix:///home/rails/sistema_hotel/shared/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
 set :puma_preload_app, true
