1ffcb72b6ad467ada5896f65d0853bae7f2237fc
Rodrigo Teixeira <rodrigotnoronha@gmail.com>
2016-12-04 22:33:08 -0300

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :branch, :master
 set :user, 'rails'
 set :deploy_to, '/home/rails/sistema_hotel'
 set :pty, true
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
 set :keep_releases, 5
 set :rvm_type, :user
