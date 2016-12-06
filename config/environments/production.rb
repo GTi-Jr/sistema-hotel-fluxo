c7f20dca9fd787fdd849c22bc9526b9c3dcfdc0c
Patrick Magal <pixelzip0@gmail.com>
2016-12-06 01:28:32 -0300

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,13 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: Rails.application.secrets.site_domain }
   
-  ActionMailer::Base.smtp_settings = {
-    :address => "smtp.sendgrid.net",
-    :port => 587,
-    :domain => '138.197.19.102',
-    :authentication => :plain,
-    :user_name => ENV["SENDGRID_USERNAME"],
-    :password => ENV["SENDGRID_PASSWORD"]
-}
+
 end
