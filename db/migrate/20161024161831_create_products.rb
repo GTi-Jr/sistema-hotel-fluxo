215b9c417f88266f39618f25cd6f4b8c527df5ee
Patrick Magal <pixelzip0@gmail.com>
2016-12-07 15:24:08 -0300

    varchar code

--- db/migrate/20161024161831_create_products.rb
+++ db/migrate/20161024161831_create_products.rb
@@ -1,7 +1,7 @@
 class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
-      t.integer :code
+      t.string :code
       t.string :name
       t.text :description
       t.decimal  "price", precision: 8, scale: 2, default: 0.0, default: nil
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,9 @@
 # NÃO RODAR ESSE ARQUIVO APÓS ENTRAR EM PRODUÇÃO. **********************
 Sector.create(name: 'Todos')
-Sector.create(name: 'Portaria')
-Sector.create(name: 'Garçons')
 Department.create(name: 'Todos')
-Department.create(name: 'Cozinha')
-Department.create(name: 'Recepçao')
-Product.create(name: 'Hospedagem', code: 555, type_t: 'sale')
+Product.create(name: 'Hospedagem', code: 001, type_t: 'sale')
+Product.create(name: 'Alivio', code: 1000, type_t: 'sale')
+Product.create(name: 'Suprimento', code: 2000, type_t: 'sale')
 Employee.create(name: 'Admin GTI', email: 'admin@hotel.com', password: Rails.application.secrets.admin_email, sector_id: '1', department_id: '1', code: '123', admin: true)
 # Inicializa o dinheiro em caixa
 begin
