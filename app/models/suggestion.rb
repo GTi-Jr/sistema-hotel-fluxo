edd51a0edcd562e4c38b1a764434dd2d4bb35cc0
Patrick Magal <pixelzip0@gmail.com>
2016-12-19 15:21:31 -0300

    limite suggest

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -1,7 +1,7 @@
 class Suggestion
   def self.main_query(options = {})
 	  products_suggestions = Product.where(type_t: [Product.type_ts[options[:product_type_t].to_sym], Product.type_ts[:both]])
-                                  .where("code LIKE ? OR name LIKE ?", "#{options[:queryString]}%", "#{options[:queryString]}%").limit(5)
+                                  .where("code LIKE ? OR name LIKE ?", "#{options[:queryString]}%", "#{options[:queryString]}%").limit(15)
 	  
     products_suggestions.any? ? products_suggestions : false
   end
