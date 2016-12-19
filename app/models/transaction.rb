10f5c7745eef523535430bb776798379e88e8dc9
Patrick Magal <pixelzip0@gmail.com>
2016-12-19 13:37:55 -0300

    bug ok?

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -55,7 +55,7 @@ class Transaction < ApplicationRecord
 
   def set_price
     unless product.name == 'Hospedagem' || product.name == 'Suprimento' || product.name == 'Alivio'
-      self.price = quantity * product.price
+      self.price = (quantity.to_i * product.price.to_f)
     end
   end
 
